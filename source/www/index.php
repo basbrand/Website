7c5b515ca287a22b3779a8a9c23e06c683063f3d
Babs Gösgens <babs@teemr.com>
2016-09-30 15:54:26 +0200

    Favicon and description tag

--- source/www/index.php
+++ source/www/index.php
@@ -26,7 +26,13 @@ $high_res = "(-webkit-min-device-pixel-ratio: 1.5), (min-resolution: 144dpi)";
 <head>
   <meta charset="UTF-8">
   <title>Brickson</title>
+  <meta name="description" content="Brickson is er voor organisaties die de volgende stap in digitaal willen maken. Werken met Brickson betekent samen ontdekken, bedenken, ontwerpen of bouwen."/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/manifest.json">
   <link rel="stylesheet" type="text/css" href="/styles/screen.css?id=1474118468489">
   <script src="https://use.typekit.net/zaf1yku.js"></script>
   <script src="/javascript/html5shiv.min.js"></script>
