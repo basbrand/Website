7f815abc1c85712d926d97b102da51f12f42988b
Babs Gösgens <babs@teemr.com>
2016-09-30 16:55:59 +0200

    GA Tracking code

--- build/www/index.php
+++ build/www/index.php
@@ -20,6 +20,18 @@ $gt_large_desktop = "(min-width: 1600px)";
 $portrait = "(orientation: portrait)";
 $landscape = "(orientation: landscape)";
 $high_res = "(-webkit-min-device-pixel-ratio: 1.5), (min-resolution: 144dpi)";
+
+$extension = pathinfo($_SERVER['SERVER_NAME'], PATHINFO_EXTENSION);
+$analytics_code = '';
+switch($extension) {
+    case 'nl':
+        $analytics_code = 'UA-84990043-1';
+        break;
+    case 'eu':
+        $analytics_code = 'UA-84990043-2';
+        break;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -41,6 +53,14 @@ $high_res = "(-webkit-min-device-pixel-ratio: 1.5), (min-resolution: 144dpi)";
   <script src="/javascript/picturefill.min.js" async></script>
   <script src="/javascript/pf.intrinsic.min.js" async></script>
   <base href="/"><style type="text/css">@font-face{font-family:futura-pt;src:url(https://fonts.typekit.net/af/9749f0/00000000000000000001008f/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n5) format("woff2");font-weight:500;font-style:normal;}@font-face{font-family:futura-pt;src:url(https://fonts.typekit.net/af/8a5494/000000000000000000013365/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4) format("woff2");font-weight:400;font-style:normal;}@font-face{font-family:futura-pt;src:url(https://fonts.typekit.net/af/75f41e/000000000000000000010096/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3) format("woff2");font-weight:300;font-style:normal;}@font-face{font-family:merriweather;src:url(https://fonts.typekit.net/af/f20d56/000000000000000000014a3e/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4) format("woff2");font-weight:400;font-style:normal;}@font-face{font-family:merriweather;src:url(https://fonts.typekit.net/af/f87f0c/000000000000000000014a3d/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i4) format("woff2");font-weight:400;font-style:italic;}</style>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', '<?php echo $analytics_code; ?>', 'auto');
+    ga('send', 'pageview');
+  </script>
 </head>
   <body class="ddebug-layout landingpage">
     <section class="panel is-active">
