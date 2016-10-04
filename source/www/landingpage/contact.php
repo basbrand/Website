fc343fafe92f0dcf89db3346757337f1e55cdba0
Babs Gösgens <babs@teemr.com>
2016-10-04 14:11:43 +0200

    Contact details

--- source/www/landingpage/contact.php
+++ source/www/landingpage/contact.php
@@ -14,57 +14,30 @@
   </div>
   <div class="well offices">
       <div class="grid grid--tile">
-        <figure class="grid__cell tile tile--shop cover">
-          <img
-            class="cover__image"
-            alt=""
-            src="/images/content/brickson_amsterdam--tile--smartphone.jpg"
-            srcset="
-              /images/content/brickson_amsterdam--tile--tablet--2x.jpg 2x,
-              /images/content/brickson_amsterdam--tile--tablet.jpg 320w,
-              /images/content/brickson_amsterdam--tile--smartphone.jpg 240w,
-              /images/content/brickson_amsterdam--tile--smartphone--2x.jpg 240w,
-              /images/content/brickson_amsterdam--tile--smartphone--2x.jpg 2x">
-              <figcaption class="tile__body is-narrative">
-                <p>Kantoor Brickson Amsterdam</p>
-              </figcaption>
-        </figure>
-        <figure class="grid__cell tile tile--shop cover">
-          <img
-            class="cover__image"
-            alt=""
-            src="/images/content/brickson_den-bosch--tile--smartphone.jpg"
-            srcset="
-              /images/content/brickson_den-bosch--tile--tablet--2x.jpg 2x,
-              /images/content/brickson_den-bosch--tile--tablet.jpg 320w,
-              /images/content/brickson_den-bosch--tile--smartphone.jpg 240w,
-              /images/content/brickson_den-bosch--tile--smartphone--2x.jpg 240w,
-              /images/content/brickson_den-bosch--tile--smartphone--2x.jpg 2x">
-          <figcaption class="tile__body is-narrative">
-            <p>Kantoor Brickson Den Bosch</p>
-          </figcaption>
-        </figure>
-        <div class="grid__cell tile gutters">
-          <h3 class="brand brand--ams leader--tiny trailer--tiny">
+        <div class="grid__cell tile tile--shop">
+          <h3 class="tile__body brand brand--ams leader--tiny trailer--tiny">
             <span class="is-narrative">Brickson Amsterdam</span>
           </h3>
+        </div>
+        <div class="grid__cell tile tile--shop">
+          <h3 class="tile__body brand brand--dnb leader--tiny trailer--tiny">
+            <span class="is-narrative">Brickson Den Bosch</span>
+          </h3>
+        </div>
+        <div class="grid__cell tile">
           <address class="gutters">
-            <p class="body--whisper">Amsteldijk&nbsp;128<br/>
-            1087 RT Amsterdam</p>
             <p class="body--whisper"><a class="anchor--shy anchor--reset body--number" href="tel:0031202610120"><em>(020) 26 101 20</em></a></p>
             <p><a class="anchor--shy body--email" href="mailto:amsterdam@brickson.nl?subject=Samen%20stappen%20zetten"><em>amsterdam@brickson.nl</em></a></p>
-
+            <p class="body--whisper leader">Amsteldijk&nbsp;128<br/>
+            1087 RT Amsterdam</p>
           </address>
         </div>
         <div class="grid__cell tile">
-          <h3 class="brand brand--dnb leader--tiny trailer--tiny">
-            <span class="is-narrative">Brickson Den Bosch</span>
-          </h3>
           <address class="gutters">
-            <p class="body--whisper">Burg<span class="is-lt-large-handheld">.</span><span class="is-gt-large-handheld">emeester</span>&nbsp;Burgerslaan&nbsp;40a<br/>
-            5245 NH Rosmalen</p>
             <p class="body--whisper"><a class="anchor--shy anchor--reset body--number" href="tel:003173123456"><em>(073) 203 22 76</em></a></p>
             <p><a class="anchor--shy body--email" href="mailto:denbosch@brickson.nl?subject=Samen%20stappen%20zetten"><em>denbosch@brickson.nl</em></a></p>
+            <p class="body--whisper leader">Burg<span class="is-lt-large-handheld">.</span><span class="is-gt-large-handheld">emeester</span>&nbsp;Burgerslaan&nbsp;40a<br/>
+            5245 NH Rosmalen</p>
           </address>
         </div>
       </div>
