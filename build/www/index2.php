8946a384373e5bd86ee48c943a9affc79afc2e32
Babs Gösgens <babs@teemr.com>
2016-10-07 12:05:17 +0200

    Tools intro

---
+++ build/www/index2.php
@@ -0,0 +1,18 @@
+<?php include 'include.php'; ?>
+<!DOCTYPE html>
+<html lang="nl">
+  <?php include 'head.php'; ?>
+  <body class="<?php echo $debug ? 'debug-layout ' : ''; ?>p-landingpage">
+    <section class="panel is-active">
+      <?php include 'landingpage/handshake.php'; ?>
+      <?php include 'landingpage/invitation.php'; ?>
+      <?php include 'testimonials/eigenhuis.php'; ?>
+      <?php include 'landingpage/introduction.php'; ?>
+    </section>
+    <?php include 'landingpage/tools.php'; ?>
+    <section>
+      <?php include 'testimonials/evean.php'; ?>
+      <?php include 'landingpage/contact.php'; ?>
+    </section>
+  </body>
+</html>
