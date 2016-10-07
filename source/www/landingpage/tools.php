5544f2e2594505ec971c7c26b3b0b9a493115b9a
Babs Gösgens <babs@teemr.com>
2016-10-07 12:03:18 +0200

    Tools intro

--- source/www/landingpage/tools.php
+++ source/www/landingpage/tools.php
@@ -3,7 +3,7 @@
     <header class="well__title" style="width: 100%;">
       <!--em class="title title--section trailer--hero">Onze winkels</em-->
       <h2 class="title leader-inside--hero">Krachtige tools voor slimme stappen</h2>
-      <p class="leader trailer">Wil je ook de volgende stap zetten waarbij jij kiest uit een aanbod van diensten die jou verder helpt? Of mogen wij je adviseren over het pad naar een nieuwe digitale ervaring? Werken met Brickson betekent samen ontdekken, bedenken, ontwerpen of bouwen.</p>
+      <p class="leader trailer">Wil je ook de volgende stap zetten? Werken met Brickson betekent samen ontdekken, bedenken, ontwerpen of bouwen. Kies uit ons aanbod diensten die jou verder helpen, of laat je adviseren over het snelste pad naar een nieuwe digitale ervaring.</p>
     </header>
   </div>
   <div class="well tools__body">
