992ad24e5b64a6800058db6712baf0c75ff8b75f
Babs Gösgens <babs@teemr.com>
2016-10-11 09:44:58 +0200

    Add breakpoint on aspect-ratio

--- build/www/index.php
+++ build/www/index.php
@@ -28,8 +28,9 @@
         </header>
         <section class="hero__body">
           <div>
-            <h2 class="title leader trailer--tiny"><strong>Vooruit met digitaal</strong></h2>
-            <p class="leader--tiny trailer">Brickson is er voor organisaties die de volgende stap in digitaal willen maken. Met een strategie, digitaal concept, website of een applicatie.</p>
+            <h2 class="title leader trailer--tiny"><strong>Een stap vooruit</strong></h2>
+            <p class="leader--tiny trailer">Je hebt ons nu al gevonden? We werken nog aan onze website!<br />Brickson is sinds 1 oktober een integrated digital agency. Onze belofte: we begrijpen mensen én techniek, en helpen je een stap vooruit te zetten in de digitale wereld.</p>
+            <p>Zie je snel. Groet <a href="http://www.teemr.works/">Teemr Works</a> en <a href="http://www.internetpeople.nl/">internetpeople</a></p>
           </div>
         </section>
       </div>
