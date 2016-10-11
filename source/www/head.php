e773fe992c23279caa3cc25026e3972ce87d9418
Babs Gösgens <babs@teemr.com>
2016-10-11 15:59:52 +0200

    Add canonical url

--- source/www/head.php
+++ source/www/head.php
@@ -4,6 +4,7 @@
   <meta name="description" content="We helpen jou met de volgende stap in digitaal. Dat betekent samen ontdekken, bedenken, ontwerpen of bouwen."/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#ffffff">
+  <link rel="canonical" href="http://www.brickson.nl/">
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
   <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
