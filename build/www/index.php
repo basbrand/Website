08fea833ae40bcd70c0b335819dff97b53f89cc8
Babs Gösgens <babs@teemr.com>
2016-10-22 00:30:16 +0200

    Tweaks

--- build/www/index.php
+++ build/www/index.php
@@ -3,16 +3,100 @@
 <html lang="nl">
   <?php include 'head.php'; ?>
   <body>
-    <nav class="navbar">
-      <ol class="menu list--reset fx" id="menu">
-      <li>
-        <a class="menu__home fx" href="javascript:void();"><span class="is-narrative">Home</span></a>
-        <a class="menu__item anchor--shy is-current is-active" href="#menu">Cases</a>
-      </li>
-      <li><a class="menu__item anchor--shy fx" href="#">Over Brickson</a></li>
-      <li><a class="menu__item anchor--shy fx is-active" href="#">Cases</a></li>
-      <li><a class="menu__item anchor--shy fx" href="#">Werken bij Brickson</a></li>
-    </ol>
-    </nav>
+    <div class="is-root">
+      <section class="pod pod--kudu pod--rhino handshake cover isolation">
+      <picture>
+        <img class="cover__image" src="https://hd.unsplash.com/photo-1428542244207-0aaec316e609">
+      </picture>
+        <div class="pod__seed">
+          <h1 class="leader trailer">
+            <img style="max-height: 100px;" src="../images/logos/Brickson-Logotype-RGB-DB.svg" alt="Brickson • Amsterdam – Den Bosch">
+          </h1>
+        </div>
+        <div class="pod__seed">
+          <h2 class="title title--inverse">Vooruit met digitaal</h2>
+          <p class="body--inverse trailer leader">Brickson is er voor organisaties die de volgende stap in digitaal willen maken. Met een strategie, digitaal concept, website of een applicatie.</p>
+        </div>
+      </section>
+      <section class="pod pod--zebra pod--rhino invitation cover isolation">
+        <div class="pod__seed seed">
+          <ol class="menu leader trailer seed__header list--reset fx bleed" id="menu">
+            <li><a class="menu__item anchor--shy fx" href="javascript:void();">Over Brickson</a></li>
+            <li><a class="menu__item anchor--shy fx is-active" href="javascript:void();">Cases</a></li>
+            <li><a class="menu__item anchor--shy fx" href="javascript:void();">Werken bij Brickson</a></li>
+          </ol>
+          <div class="seed__body">
+            <h2 class="title leader trailer">Wat is jouw volgende stap?</h2>
+            <p class="leader trailer">Werken met Brickson betekent samen ontdekken, bedenken, ontwerpen of bouwen. We lossen complexe problemen op en werken op project-, consultancy- of interimbasis.</p>
+          </div>
+          <p class="seed__footer leader trailer"><a class="button button--stretch" href="mailto:experts@brickson.nl?subject='Vooruit%20met%20Brickson'">Wil je meer weten? Neem contact op!</a></p>
+        </div>
+        <ul class="pod__seed grid">
+          <li class="grid__cell cover isolation tile">
+            <picture>
+              <img class="cover__image" src="https://hd.unsplash.com/photo-1428542244207-0aaec316e609">
+            </picture>
+            <p><strong class="title--inverse title--meta">Ontdekken</strong></p>
+            <p><strong class="title--inverse title title--section">Analyse &amp; Challenge</strong></p>
+          </li>
+          <li class="grid__cell cover isolation tile">
+            <picture>
+              <img class="cover__image" src="https://hd.unsplash.com/photo-1428542244207-0aaec316e609">
+            </picture>
+            <p><strong class="title--inverse title--meta">Ontdekken</strong></p>
+            <p><strong class="title--inverse title title--section">Analyse &amp; Challenge</strong></p>
+          </li>
+          <li class="grid__cell cover isolation tile">
+            <picture>
+              <img class="cover__image" src="https://hd.unsplash.com/photo-1428542244207-0aaec316e609">
+            </picture>
+            <p><strong class="title--inverse title--meta">Ontdekken</strong></p>
+            <p><strong class="title--inverse title title--section">Analyse &amp; Challenge</strong></p>
+          </li>
+          <li class="grid__cell cover isolation tile">
+            <picture>
+              <img class="cover__image" src="https://hd.unsplash.com/photo-1428542244207-0aaec316e609">
+            </picture>
+            <p><strong class="title--inverse title--meta">Ontdekken</strong></p>
+            <p><strong class="title--inverse title title--section">Analyse &amp; Challenge</strong></p>
+          </li>
+        </ul>
+      </section>
+      <section class="pod pod--serval pod--rhino cover isolation">
+        <picture>
+          <img class="cover__image" src="https://hd.unsplash.com/photo-1428542244207-0aaec316e609">
+        </picture>
+        <div class="pod__seed seed">
+          <h3 class="seed__header title title--section leader--hero trailer">Testimonial</h3>
+          <div class="seed__body leader trailer">
+            <blockquote class="quote"><p>Met oog en gevoel voor de mensen in je organisatie gaat Brickson altijd een stap verder.</p></blockquote>
+            <figure class="person bleed leader--hero trailer">
+              <span class="person__picture gutters">
+                <img src="/images/heads/marja_smits.jpg" class="person__image" width="56" height="56" style="background: Grey;">
+              </span>
+              <figcaption class="person__body">
+                <span class="person__name title title--section">Marja Smits</span>
+                <small class="person__occupation">Manager Strategie en Marketing Evean</small>
+              </figcaption>
+            </figure>
+          </div>
+          <div class="seed__footer leader trailer">
+            <a class="button button--stretch" href="/cases/evean/#main">Case: Evean</a>
+          </div>
+          </div>
+      </section>
+      <section class="pod pod--baboon introduction">
+        <div class="pod__seed seed isolation">
+          <h2 class="seed__header title leader trailer">Blijer, gezonder en welvarender</h2>
+          <p class="seed__body leader trailer">Brickson is er voor ambitieuze merken in traditionele markten. Wij helpen ze om relevant te blijven en meer voor klanten te betekenen. Succesvolle merken kijken vooruit en stellen zich de vraag: hoe maak ik mijn klant blijer, gezonder en welvarender?</p>
+        </div>
+        <div class="pod__seed cover isolation">
+          <picture>
+            <img class="cover__image" src="https://hd.unsplash.com/photo-1428542244207-0aaec316e609">
+          </picture>
+
+        </div>
+      </section>
+      </div>
   </body>
 </html>
