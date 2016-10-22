e417fc513026311cb5c6d8ff372a7ed0d1cae086
Babs Gösgens <babs@teemr.com>
2016-10-22 09:52:23 +0200

    Clean up

--- build/www/index.php
+++ build/www/index.php
@@ -2,37 +2,17 @@
 <!DOCTYPE html>
 <html lang="nl">
   <?php include 'head.php'; ?>
-  <body>
-    <article class="cover cover--hero">
-      <picture class="cover__image">
-        <!--[if IE 9]><video style="display: none;"><![endif]-->
-        <source sizes="(min-width: 3360px) and (orientation: landscape) 3360px, 1680px" media="(min-width: 1680px) and (orientation: landscape)" srcset="/images/content/w1680px_h1050px--hero.jpg 1680w, /images/content/w1680px_h1050px_2x--hero.jpg 3360w">
-        <source sizes="(min-width: 1280px) and (orientation: landscape) 2560px, 1280px" media="(min-width: 1280px) and (orientation: landscape)" srcset="/images/content/w1280px_h800px--hero.jpg 1280w, /images/content/w1280px_h800px_2x--hero.jpg 2560w">
-        <source sizes="(min-width: 480px) and (orientation: landscape) 1334px, 480px" media="(min-width: 480px) and (orientation: landscape)" srcset="/images/content/w667px_h375px--hero.jpg 667w, /images/content/w667px_h375px_2x--hero.jpg 1334w">
-        <source sizes="(min-width: 768px) and (orientation: portrait) 1536px, 768px" media="(min-width: 768px) and (orientation: portrait)" srcset="/images/content/w800px_h1280px--hero.jpg 768w, /images/content/w800px_h1280px_2x--hero.jpg 1536w">
-        <source sizes="(min-width: 320px) and (orientation: portrait) 640px, 320px" media="(min-width: 320px) and (orientation: portrait)" srcset="/images/content/w375px_h667px--hero.jpg 320w, /images/content/w375px_h667px_2x--hero.jpg 640w">
-        <!--[if IE 9]></video><![endif]-->
-        <img class="cover__image" src="/images/content/w1280px_h800px--hero.jpg" alt="">
-      </picture>
-      <div class="hero">
-        <header class="hero__header">
-          <div>
-            <picture class="image--stretch">
-              <!--[if IE 9]><video style="display: none;"><![endif]-->
-              <source sizes="(min-width: 280px) 280px" media="(min-width: 280px)" srcset="/images/logos/Brickson-Logotype-RGB-DB.svg 280w">
-              <!--[if IE 9]></video><![endif]-->
-              <img class="image--stretch" src="/images/logos/Brickson-Logotype-RGB-DB--tablet--2x.png" alt="">
-            </picture>
-            <h1><span class="is-narrative">Brickson - Digital agency uit Amsterdam &amp; Den Bosch</span></h1>
-          </div>
-        </header>
-        <section class="hero__body">
-          <div>
-            <h2 class="title leader trailer--tiny"><strong>De volgende stap in digitaal.</strong></h2>
-            <p class="leader--tiny trailer">Het Amsterdamse bureau Internetpeople is per 1 oktober gefuseerd met internetbureau Teemr Works uit Den Bosch. De twee bureau’s werkten al een jaar samen voor klanten als CTA, Evean, Interbank en Vereniging Eigen Huis.</p>
-          </div>
-        </section>
-      </div>
-    </article>
+  <body class="<?php echo $debug ? 'debug-layout ' : ''; ?>p-landingpage">
+    <section class="panel is-active">
+      <?php include 'landingpage/handshake.php'; ?>
+      <?php include 'landingpage/invitation.php'; ?>
+      <?php include 'testimonials/eigenhuis.php'; ?>
+      <?php include 'landingpage/introduction.php'; ?>
+    </section>
+    <?php //include 'landingpage/tools.php'; ?>
+    <section>
+      <?php include 'testimonials/evean.php'; ?>
+      <?php include 'landingpage/contact.php'; ?>
+    </section>
   </body>
 </html>
