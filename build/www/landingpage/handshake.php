8a5529978d9bf3112765c2a188214e2dbc136b63
Babs Gösgens <babs@teemr.com>
2016-10-24 16:57:10 +0200

    Fix handshake

--- build/www/landingpage/handshake.php
+++ build/www/landingpage/handshake.php
@@ -19,7 +19,7 @@
     <!--[if IE 9]></video><![endif]-->
     <img class="cover__image" src="/images/content/w320px_h576px--handshake.jpg" alt="">
   </picture>
-  <div class="pod__seed backdrop--body tile">
+  <div class="pod__seed backdrop tile">
     <div class="tile">
       <h1 class="brand brand--inverse brand--hero leader trailer"><span class="is-narrative">Brickson • Amsterdam – Den Bosch</span></h1>
     </div>
